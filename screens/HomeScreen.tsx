8780727af9162d82fe7f6209a1ae8e6c836a559f
“ShedrachJonah11” <ShedrachJonah11@gmail.com>
2024-04-29 12:27:58 +0100

    fix: edit home screen

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,27 +1,10 @@
-import {
-  ImageBackground,
-  SafeAreaView,
-  ScrollView,
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-  useWindowDimensions,
-  View,
-} from "react-native";
-import React, { useState } from "react";
-
-import { RFValue } from "react-native-responsive-fontsize";
-import COLORS from "../config/colors";
+import { StyleSheet } from "react-native";
+import React from "react";
 import Card from "../components/Card";
-import { Wifi } from "iconsax-react-native";
 import QuickAction from "../components/QuickAction";
 import RecentTransaction from "../components/RecentTransaction";
 
 const HomeScreen = () => {
-  const { width: screenWidth, height: screenHeight } = useWindowDimensions();
-
-  const height = screenWidth;
-
   return (
     <>
       <Card />
@@ -33,22 +16,4 @@ const HomeScreen = () => {
 
 export default HomeScreen;
 
-const styles = StyleSheet.create({
-  quickAction: {
-    fontFamily: "Outfit-SemiBold",
-    fontSize: RFValue(18),
-  },
-  card: {
-    backgroundColor: COLORS.white,
-    justifyContent: "center",
-    alignItems: "center",
-    gap: 10,
-    paddingHorizontal: 18,
-    paddingVertical: 15,
-    borderRadius: 10,
-  },
-  cardText: {
-    fontSize: RFValue(10),
-    fontFamily: "Outfit-Regular",
-  },
-});
+const styles = StyleSheet.create({});
