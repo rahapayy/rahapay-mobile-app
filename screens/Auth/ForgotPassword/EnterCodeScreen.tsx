00bf0216fecfb610f54c7ab7e46498f2c9e40888
“ShedrachJonah11” <ShedrachJonah11@gmail.com>
2024-05-05 06:20:04 +0100

    fix

--- screens/Auth/ForgotPassword/EnterCodeScreen.tsx
+++ screens/Auth/ForgotPassword/EnterCodeScreen.tsx
@@ -1,14 +1,14 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import React from "react";
 
 const EnterCodeScreen = () => {
   return (
-    <View>
+    <SafeAreaView>
       <Text>EnterCodeScreen</Text>
-    </View>
-  )
-}
+    </SafeAreaView>
+  );
+};
 
-export default EnterCodeScreen
+export default EnterCodeScreen;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
