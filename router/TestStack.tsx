fb4c5be901516d8d9a1aeab8d71b3ee4af2d301d
“ShedrachJonah11” <ShedrachJonah11@gmail.com>
2024-05-11 00:55:45 +0100

    feat: add test stack

---
+++ router/TestStack.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const TestStack = () => {
+  return (
+    <View>
+      <Text>TestStack</Text>
+    </View>
+  )
+}
+
+export default TestStack
+
+const styles = StyleSheet.create({})
