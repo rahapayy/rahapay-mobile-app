d5c32a5292d8bc7b109e5034d2ad48debd8403e5
“ShedrachJonah11” <ShedrachJonah11@gmail.com>
2024-05-11 01:09:48 +0100

    feat: complete reset password screen

--- screens/Auth/ForgotPassword/ResetPasswordScreen.tsx
+++ screens/Auth/ForgotPassword/ResetPasswordScreen.tsx
@@ -1,14 +1,128 @@
-import { SafeAreaView, StyleSheet, Text, View } from "react-native";
-import React from "react";
+import {
+  KeyboardAvoidingView,
+  Platform,
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import React, { useState } from "react";
+import { ArrowLeft,} from "iconsax-react-native";
+import { RFValue } from "react-native-responsive-fontsize";
+import COLORS from "../../../config/colors";
+import SPACING from "../../../config/SPACING";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import Button from "../../../components/Button";
+import FONT_SIZE from "../../../config/font-size";
 
-const ResetPasswordScreen = () => {
+const ResetPasswordScreen: React.FC<{
+  navigation: NativeStackNavigationProp<any, "">;
+}> = ({ navigation }) => {
+  const handleButtonClick = () => {
+    navigation.navigate("EnterCodeScreen");
+  };
+
   return (
     <SafeAreaView>
-      <Text>ResetPasswordScreen</Text>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View className="p-4">
+          <TouchableOpacity onPress={() => navigation.goBack()}>
+            <ArrowLeft color="#000" />
+          </TouchableOpacity>
+
+          <View className="mt-4">
+            <Text style={styles.headText}>Reset Password</Text>
+            <Text style={styles.subText}>
+              Enter your email address to receive code
+            </Text>
+          </View>
+          <KeyboardAvoidingView
+            style={{ flex: 1 }}
+            behavior={Platform.OS === "ios" ? "padding" : undefined}
+            keyboardVerticalOffset={Platform.OS === "ios" ? -50 : 0}
+          >
+            <View className="mt-10">
+              <Text style={styles.label}>Email Address</Text>
+              <TextInput
+                style={styles.textInput}
+                placeholder="Enter your email address"
+                placeholderTextColor={"#DFDFDF"}
+              />
+            </View>
+
+            <Button
+              title={"Reset Password"}
+              onPress={handleButtonClick}
+              style={styles.proceedButton}
+            />
+          </KeyboardAvoidingView>
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 };
 
 export default ResetPasswordScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  headText: {
+    fontFamily: "Outfit-Medium",
+    fontSize: RFValue(24),
+    marginBottom: 10,
+  },
+  subText: {
+    fontFamily: "Outfit-Regular",
+    fontSize: RFValue(16),
+  },
+  textInput: {
+    borderWidth: 1,
+    borderRadius: 10,
+    borderColor: "#DFDFDF",
+    padding: 18,
+  },
+  label: {
+    fontFamily: "Outfit-Regular",
+    marginBottom: 10,
+    fontSize: RFValue(14),
+  },
+  vertical: {
+    backgroundColor: COLORS.black100,
+    width: 1,
+    height: "100%",
+    marginHorizontal: SPACING,
+  },
+  inputContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    fontSize: RFValue(14),
+    borderRadius: 10,
+    padding: 18,
+    width: "100%",
+    borderWidth: 1,
+    borderColor: "#DFDFDF",
+  },
+  input: {
+    flex: 1,
+    height: "100%",
+    fontSize: RFValue(14),
+  },
+  numberText: {
+    fontFamily: "Outfit-Regular",
+  },
+  proceedButton: {
+    marginTop: SPACING * 4,
+  },
+  proceedButtonText: {
+    fontFamily: "Outfit-Regular",
+    color: "#fff",
+    fontSize: RFValue(16),
+  },
+  forgotPasswordText: {
+    fontFamily: "Outfit-Regular",
+    color: COLORS.violet600,
+    fontSize: FONT_SIZE.medium,
+  },
+});
