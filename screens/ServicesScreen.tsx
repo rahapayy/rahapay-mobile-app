0b8faad498bc5d137cf448e08e0c73b51e01e67f
“ShedrachJonah11” <ShedrachJonah11@gmail.com>
2024-05-12 13:34:25 +0100

    fix

--- screens/ServicesScreen.tsx
+++ screens/ServicesScreen.tsx
@@ -1,14 +1,24 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { SafeAreaView, ScrollView, StyleSheet, Text, View } from "react-native";
+import React from "react";
+import FONT_SIZE from "../config/font-size";
 
 const ServicesScreen = () => {
   return (
-    <View>
-      <Text>ServicesScreen</Text>
-    </View>
-  )
-}
+    <SafeAreaView style={{ flex: 1 }}>
+      <ScrollView>
+        <View className="p-6">
+          <Text style={styles.headText}>Services</Text>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
 
-export default ServicesScreen
+export default ServicesScreen;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  headText: {
+    fontFamily: "Outfit-Regular",
+    fontSize: FONT_SIZE.large,
+  },
+});
