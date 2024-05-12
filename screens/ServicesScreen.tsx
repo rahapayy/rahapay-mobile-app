ec3f38112fb5aa1d2ee770473de266acb10017bf
“ShedrachJonah11” <ShedrachJonah11@gmail.com>
2024-05-12 13:55:27 +0100

    feat: layout

--- screens/ServicesScreen.tsx
+++ screens/ServicesScreen.tsx
@@ -1,6 +1,17 @@
-import { SafeAreaView, ScrollView, StyleSheet, Text, View } from "react-native";
+import {
+  Image,
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
 import React from "react";
 import FONT_SIZE from "../config/font-size";
+import Airtime from "../assets/svg/smartphone-rotate-angle_svgrepo.com.svg";
+import Tv from "../assets/svg/tv_svgrepo.com.svg";
+import Electricity from "../assets/svg/electricity_svgrepo.com.svg";
+import Data from "../assets/svg/signal_svgrepo.com.svg";
 
 const ServicesScreen = () => {
   return (
@@ -8,6 +19,35 @@ const ServicesScreen = () => {
       <ScrollView>
         <View className="p-6">
           <Text style={styles.headText}>Services</Text>
+          {/* Card */}
+          <View className="w-full bg-white p-6 mt-10 rounded-xl">
+            <View className="flex-row gap-12 justify-center">
+              <View className="items-center">
+                <View className=" bg-[#EEEBF9] rounded-full flex-shrink-0 w-10 h-10 items-center justify-center">
+                  <Airtime />
+                </View>
+                <Text>Airtime</Text>
+              </View>
+              <View className="items-center">
+                <View className=" bg-[#EEEBF9] rounded-full flex-shrink-0 w-10 h-10 items-center justify-center">
+                  <Data />
+                </View>
+                <Text>Airtime</Text>
+              </View>
+              <View className="items-center">
+                <View className=" bg-[#EEEBF9] rounded-full flex-shrink-0 w-10 h-10 items-center justify-center">
+                  <Tv />
+                </View>
+                <Text>Airtime</Text>
+              </View>
+              <View className="items-center">
+                <View className=" bg-[#EEEBF9] rounded-full flex-shrink-0 w-10 h-10 items-center justify-center">
+                  <Electricity />
+                </View>
+                <Text>Airtime</Text>
+              </View>
+            </View>
+          </View>
         </View>
       </ScrollView>
     </SafeAreaView>
