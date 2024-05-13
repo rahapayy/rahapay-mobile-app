cbfcc522c362209f07e046c006e3fc15695845d7
“ShedrachJonah11” <ShedrachJonah11@gmail.com>
2024-05-13 20:23:49 +0100

    feat: add TransactionHistoryScreen

---
+++ screens/TransactionHistoryScreen.tsx
@@ -0,0 +1,14 @@
+import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const TransactionHistoryScreen = () => {
+  return (
+    <SafeAreaView>
+      <Text>TransactionHistoryScreen</Text>
+    </SafeAreaView>
+  );
+};
+
+export default TransactionHistoryScreen;
+
+const styles = StyleSheet.create({});
