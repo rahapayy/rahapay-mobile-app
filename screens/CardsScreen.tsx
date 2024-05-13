06fd1402109801c87427494ecd636758a1ad3a2c
“ShedrachJonah11” <ShedrachJonah11@gmail.com>
2024-05-13 22:01:32 +0100

    feat: complete

--- screens/CardsScreen.tsx
+++ screens/CardsScreen.tsx
@@ -1,6 +1,7 @@
 import { SafeAreaView, ScrollView, StyleSheet, Text, View } from "react-native";
 import React from "react";
 import FONT_SIZE from "../config/font-size";
+import ComingSoon from "../assets/svg/Coming Soon.svg";
 
 const CardsScreen = () => {
   return (
@@ -8,6 +9,13 @@ const CardsScreen = () => {
       <ScrollView>
         <View className="p-6">
           <Text style={styles.headText}>Create Virtual Card</Text>
+        </View>
+
+        <View className="justify-center items-center">
+          <ComingSoon />
+          <Text style={styles.comingsoonText}>
+            We are creating something amazing. Stay tuned!
+          </Text>
         </View>
       </ScrollView>
     </SafeAreaView>
@@ -20,5 +28,10 @@ const styles = StyleSheet.create({
   headText: {
     fontFamily: "Outfit-Regular",
     fontSize: FONT_SIZE.large,
+  },
+  comingsoonText: {
+    fontFamily: "Outfit-Regular",
+    fontSize: FONT_SIZE.medium,
+    textAlign: "center",
   },
 });
