e18229bfa0f177fd99ee346a85a64ffea8592e69
“ShedrachJonah11” <ShedrachJonah11@gmail.com>
2024-05-18 23:29:45 +0100

    feat: add EnableNotificationScreen header

--- screens/Profile/EnableNotificationScreen.tsx
+++ screens/Profile/EnableNotificationScreen.tsx
@@ -1,14 +1,120 @@
-import { StyleSheet, Text, View } from "react-native";
+import {
+  Image,
+  Platform,
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import React from "react";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import { ArrowLeft, DocumentDownload } from "iconsax-react-native";
+import SPACING from "../../config/SPACING";
+import FONT_SIZE from "../../config/font-size";
+import COLORS from "../../config/colors";
+import Airtel from "../assets/svg/airtel.svg";
+import { RFValue } from "react-native-responsive-fontsize";
 
-const EnableNotificationScreen = () => {
+const EnableNotificationScreen: React.FC<{
+  navigation: NativeStackNavigationProp<any, "">;
+}> = ({ navigation }) => {
   return (
-    <View>
-      <Text>EnableNotificationScreen</Text>
-    </View>
+    <SafeAreaView style={{ flex: 1 }}>
+      <ScrollView>
+        <View>
+          <View style={styles.header}>
+            <TouchableOpacity
+              onPress={() => navigation.goBack()}
+              style={styles.leftIcon}
+            >
+              <ArrowLeft color={"#000"} size={24} />
+            </TouchableOpacity>
+            <Text style={[styles.headerText]} allowFontScaling={false}>
+              Notification
+            </Text>
+          </View>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
 export default EnableNotificationScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  header: {
+    flexDirection: "row",
+    alignItems: "center",
+    paddingHorizontal: SPACING * 2,
+    paddingTop: Platform.OS === "ios" ? SPACING * 2 : SPACING * 2,
+    paddingBottom: SPACING * 3,
+  },
+  leftIcon: {
+    marginRight: SPACING,
+  },
+  headerText: {
+    color: "#000",
+    fontSize: FONT_SIZE.medium,
+    fontFamily: "Outfit-Regular",
+    flex: 1,
+  },
+  headerTextDark: {
+    color: COLORS.white,
+  },
+  headTextContainer: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  itemText: {
+    fontSize: FONT_SIZE.medium,
+    fontFamily: "Outfit-Medium",
+    paddingVertical: SPACING * 2,
+  },
+  container: {
+    backgroundColor: COLORS.white,
+    paddingHorizontal: SPACING,
+    paddingVertical: SPACING,
+    borderRadius: SPACING,
+    marginTop: SPACING * 4,
+  },
+  headText: {
+    fontFamily: "Outfit-Regular",
+    fontSize: RFValue(18),
+    marginBottom: SPACING,
+  },
+  titleText: {
+    fontFamily: "Outfit-Regular",
+    fontSize: RFValue(16),
+    paddingVertical: SPACING,
+  },
+  descriptionText: {
+    fontFamily: "Outfit-Regular",
+    fontSize: RFValue(14),
+    color: "#9BA1A8",
+  },
+  completedText: {
+    fontFamily: "Outfit-Regular",
+    color: "#06C270",
+  },
+  button: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    borderWidth: 1,
+    borderStyle: "dotted",
+    borderColor: "#000",
+    paddingVertical: SPACING,
+    paddingHorizontal: SPACING * 3,
+    borderRadius: 8,
+    marginTop: SPACING * 2,
+  },
+  buttonText: {
+    marginLeft: SPACING,
+    color: "#000",
+    fontFamily: "Outfit-Regular",
+    fontSize: FONT_SIZE.medium,
+  },
+});
