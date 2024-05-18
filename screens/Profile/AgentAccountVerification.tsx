dd8f80699f98de0d493c1fc31c55c51be1e161c2
“ShedrachJonah11” <ShedrachJonah11@gmail.com>
2024-05-18 22:53:54 +0100

    feat: add profile floe

---
+++ screens/Profile/AgentAccountVerification.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const AgentAccountVerification = () => {
+  return (
+    <View>
+      <Text>AgentAccountVerification</Text>
+    </View>
+  );
+};
+
+export default AgentAccountVerification;
+
+const styles = StyleSheet.create({});
---
+++ screens/Profile/EnableNotificationScreen.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const EnableNotificationScreen = () => {
+  return (
+    <View>
+      <Text>EnableNotificationScreen</Text>
+    </View>
+  );
+};
+
+export default EnableNotificationScreen;
+
+const styles = StyleSheet.create({});
