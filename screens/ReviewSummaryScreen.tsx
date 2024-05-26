7b36538f8f09ce8488dbbee768506c40776c7b80
“ShedrachJonah11” <ShedrachJonah11@gmail.com>
2024-05-26 17:28:42 +0100

    feat: add review summary screen

---
+++ screens/ReviewSummaryScreen.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const ReviewSummaryScreen = () => {
+  return (
+    <View>
+      <Text>ReviewSummaryScreen</Text>
+    </View>
+  );
+};
+
+export default ReviewSummaryScreen;
+
+const styles = StyleSheet.create({});
