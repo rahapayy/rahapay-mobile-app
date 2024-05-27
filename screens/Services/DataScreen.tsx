bc61254f5a411c545f657d315e3d1ee786fc91af
“ShedrachJonah11” <ShedrachJonah11@gmail.com>
2024-05-27 03:21:37 +0100

    feat: complete datascreen

--- screens/Services/DataScreen.tsx
+++ screens/Services/DataScreen.tsx
@@ -1,25 +1,40 @@
+import React, { useState } from "react";
 import {
-  Image,
-  Platform,
   SafeAreaView,
   ScrollView,
   StyleSheet,
   Text,
   TouchableOpacity,
   View,
+  Platform,
+  TextInput,
+  Switch,
 } from "react-native";
-import React from "react";
 import { NativeStackNavigationProp } from "@react-navigation/native-stack";
-import { ArrowLeft, DocumentDownload } from "iconsax-react-native";
+import {
+  ArrowDown,
+  ArrowDown2,
+  ArrowLeft,
+  Profile,
+  ProfileCircle,
+} from "iconsax-react-native";
 import SPACING from "../../config/SPACING";
 import FONT_SIZE from "../../config/font-size";
 import COLORS from "../../config/colors";
-import Airtel from "../assets/svg/airtel.svg";
 import { RFValue } from "react-native-responsive-fontsize";
+import Airtel from "../../assets/svg/air.svg";
+import Mtn from "../../assets/svg/mtn.svg";
+import Eti from "../../assets/svg/eti.svg";
+import Glo from "../../assets/svg/glo.svg";
+import Button from "../../components/Button";
 
 const DataScreen: React.FC<{
   navigation: NativeStackNavigationProp<any, "">;
 }> = ({ navigation }) => {
+  const [amount, setAmount] = useState("");
+
+  const amounts = [50, 100, 200, 500, 1000, 2000, 3000, 5000];
+
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <ScrollView>
@@ -34,6 +49,104 @@ const DataScreen: React.FC<{
             <Text style={[styles.headerText]} allowFontScaling={false}>
               Buy Data Bundle
             </Text>
+          </View>
+          <View className="px-6">
+            <View style={styles.tabContent}>
+              <View>
+                <Text style={styles.headText} allowFontScaling={false}>
+                  Saved Beneficiaries
+                </Text>
+                <View className="flex-row mb-4 gap-2">
+                  <View className="bg-[#EEEBF9] p-3 rounded-2xl">
+                    <Text>My number</Text>
+                  </View>
+                  <View className="bg-[#EEEBF9] p-3 rounded-2xl">
+                    <Text>+234 0862753934</Text>
+                  </View>
+                </View>
+
+                {/* Select Network Provider */}
+                <Text style={styles.headText} allowFontScaling={false}>
+                  Select Network Provider
+                </Text>
+                <View className="flex-row p-2 bg-white rounded-xl  items-center justify-between">
+                  <TouchableOpacity>
+                    <Airtel />
+                  </TouchableOpacity>
+                  <TouchableOpacity>
+                    <Mtn />
+                  </TouchableOpacity>
+                  <TouchableOpacity>
+                    <Eti />
+                  </TouchableOpacity>
+                  <TouchableOpacity>
+                    <Glo />
+                  </TouchableOpacity>
+                </View>
+
+                {/* Inputs */}
+                <View>
+                  <View className="mt-4">
+                    <Text style={styles.label} allowFontScaling={false}>
+                      Phone Number
+                    </Text>
+                    <View style={styles.inputContainer}>
+                      <TextInput
+                        style={styles.input}
+                        placeholder="Enter phone number"
+                        placeholderTextColor="#BABFC3"
+                        allowFontScaling={false}
+                      />
+                      <TouchableOpacity>
+                        <ProfileCircle color={COLORS.violet400} />
+                      </TouchableOpacity>
+                    </View>
+                  </View>
+                  <View className="mt-4">
+                    <Text style={styles.label} allowFontScaling={false}>
+                      Plan
+                    </Text>
+                    <TouchableOpacity style={styles.inputContainer}>
+                      <Text
+                        style={{
+                          flex: 1,
+                          fontFamily: "Outfit-Regular",
+                          color: "#DFDFDF",
+                          fontSize: RFValue(12),
+                        }}
+                        allowFontScaling={false}
+                      >
+                        Select plan
+                      </Text>
+                      <ArrowDown2 color="#000" />
+                    </TouchableOpacity>
+                  </View>
+                </View>
+
+                {/* Save as Beneficiary Toogle */}
+                <View className="mb-4">
+                  <View className="flex-row items-center gap-2 mt-2">
+                    <Text
+                      style={styles.beneficiaryText}
+                      allowFontScaling={false}
+                    >
+                      Save as beneficiary
+                    </Text>
+                    <Switch />
+                  </View>
+                </View>
+              </View>
+            </View>
+
+            <Button
+              title={"Proceed"}
+              style={{
+                backgroundColor: COLORS.violet200,
+              }}
+              onPress={() => {
+                navigation.navigate("ReviewSummaryScreen");
+              }}
+            />
           </View>
         </View>
       </ScrollView>
@@ -60,61 +173,75 @@ const styles = StyleSheet.create({
     fontFamily: "Outfit-Regular",
     flex: 1,
   },
-  headerTextDark: {
-    color: COLORS.white,
+  tabsContainer: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    marginVertical: SPACING,
   },
-  headTextContainer: {
-    flex: 1,
+  tab: {
     alignItems: "center",
-    justifyContent: "center",
+    flex: 1,
   },
-  itemText: {
-    fontSize: FONT_SIZE.medium,
-    fontFamily: "Outfit-Medium",
-    paddingVertical: SPACING * 2,
+  tabText: {
+    fontSize: RFValue(14),
+    color: "#9BA1A8",
+    fontFamily: "Outfit-Regular",
   },
-  container: {
-    backgroundColor: COLORS.white,
-    paddingHorizontal: SPACING,
-    paddingVertical: SPACING,
-    borderRadius: SPACING,
-    marginTop: SPACING * 4,
+  activeTab: {
+    alignItems: "center",
+  },
+  activeTabText: {
+    color: COLORS.violet400,
+  },
+  activeTabIndicator: {
+    height: 2,
+    backgroundColor: COLORS.violet400,
+    marginTop: 4,
+    width: "100%",
+  },
+  tabContent: {
+    marginTop: SPACING,
+    fontFamily: "Outfit-Regular",
+  },
+  contentText: {
+    fontSize: RFValue(18),
+    fontFamily: "Outfit-Regular",
+    color: "#000",
   },
   headText: {
     fontFamily: "Outfit-Regular",
-    fontSize: RFValue(18),
+    fontSize: RFValue(12),
     marginBottom: SPACING,
   },
-  titleText: {
+  label: {
     fontFamily: "Outfit-Regular",
-    fontSize: RFValue(16),
-    paddingVertical: SPACING,
+    marginBottom: 10,
+    fontSize: RFValue(12),
   },
-  descriptionText: {
-    fontFamily: "Outfit-Regular",
-    fontSize: RFValue(14),
-    color: "#9BA1A8",
-  },
-  completedText: {
-    fontFamily: "Outfit-Regular",
-    color: "#06C270",
-  },
-  button: {
+  inputContainer: {
     flexDirection: "row",
     alignItems: "center",
-    justifyContent: "center",
+    fontSize: RFValue(12),
+    borderRadius: 10,
+    padding: 18,
+    width: "100%",
     borderWidth: 1,
-    borderStyle: "dotted",
-    borderColor: "#000",
-    paddingVertical: SPACING,
-    paddingHorizontal: SPACING * 3,
-    borderRadius: 8,
-    marginTop: SPACING * 2,
+    borderColor: "#DFDFDF",
   },
-  buttonText: {
-    marginLeft: SPACING,
-    color: "#000",
+  input: {
+    flex: 1,
+    height: "100%",
+    fontSize: RFValue(12),
     fontFamily: "Outfit-Regular",
-    fontSize: FONT_SIZE.medium,
+  },
+  topupText: {
+    fontFamily: "Outfit-Regular",
+  },
+  amountText: {
+    fontFamily: "Outfit-Regular",
+  },
+  beneficiaryText: {
+    fontFamily: "Outfit-Regular",
+    fontSize: RFValue(12),
   },
 });
