81de7ed392c157716d126146ac69850f53d92281
“ShedrachJonah11” <ShedrachJonah11@gmail.com>
2024-05-27 02:18:00 +0100

    feat: add TransactionCompletedScreen

---
+++ screens/TransactionCompletedScreen.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const TransactionCompletedScreen = () => {
+  return (
+    <View>
+      <Text>TransactionCompletedScreen</Text>
+    </View>
+  );
+};
+
+export default TransactionCompletedScreen;
+
+const styles = StyleSheet.create({});
