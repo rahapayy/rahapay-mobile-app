34aee4268d73ce714f0f9352276006158eed081e
“ShedrachJonah11” <ShedrachJonah11@gmail.com>
2024-07-08 09:06:51 +0100

    feat: integrate use wallet function

--- hooks/use-wallet.tsx
+++ hooks/use-wallet.tsx
@@ -4,27 +4,36 @@ import { AuthContext } from "../context/AuthContext";
 
 const useWallet = () => {
   const { userInfo } = useContext(AuthContext);
-  const { data, ...rest } = useSWR(`/users/${userInfo?.data?.user?.id}`);
+  const { data, ...rest } = useSWR(`user/dashboard/me/`);
 
-  const balance = React.useMemo(() => data?.data?.wallet?.balance || 0, [data]);
+  console.log(data);
 
-  const account: {
-    accountNumber: number;
-    bankName: string;
-    currencyCode: string;
-  } = React.useMemo(() => data?.data?.payment_account[0], [data]);
+  const balance = React.useMemo(() => data?.wallet?.balance || 0, [data]);
 
-  const transactions: {
-    amount: number;
-    created_at: string;
-    id: string;
-    ownerId: string;
-    purpose: string;
-    referenceId: string;
-    status: string;
-    tranxType: string;
-    updated_at: string;
-  }[] = React.useMemo(() => data?.data?.transaction, [data]);
+  const account = React.useMemo(() => {
+    if (data?.payment_account && data.payment_account.length > 0) {
+      return {
+        accountNumber: data.payment_account[0].accountNumber,
+        bankName: data.payment_account[0].bankName,
+        currencyCode: data.payment_account[0].currencyCode
+      };
+    }
+    return { accountNumber: null, bankName: "", currencyCode: "" };
+  }, [data]);
+
+  const transactions = React.useMemo(() => {
+    return data?.transaction?.map((trx: { amount: any; created_at: any; _id: any; ownerId: any; purpose: any; referenceId: any; status: any; tranxType: any; updated_at: any; }) => ({
+      amount: trx.amount || 0,
+      created_at: trx.created_at || "",
+      id: trx._id,
+      ownerId: trx.ownerId || "",
+      purpose: trx.purpose || "",
+      referenceId: trx.referenceId || "",
+      status: trx.status || "",
+      tranxType: trx.tranxType || "",
+      updated_at: trx.updated_at || ""
+    })) || [];
+  }, [data]);
 
   return { balance, account, transactions, ...rest };
 };
